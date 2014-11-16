750ff9a3254eeec7ae56a969441809eb984f7f7e
pascal hausberger <pascalh7@hotmail.com>
2014-11-16 12:45:19 +0100

    Changed Send button to black, gave Coming Soon block a bit bigger padding, added jQuery to show Thank You note

--- js/thanks.js
+++ js/thanks.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $("#thanks").click(function(){
-  	$( "#thanks" )
-  		.html( "<div class="row"><p class="WHAT">Thank you!</p></div>" );
+  $("#submit").click(function(){
+  	$("#thanks").append("<div class='col-sm-4 col-sm-offset-4 hidden-xs'><br><p class='thankyou'>Thank you!</p></div>");
+  	$("#thanks").append("<div class='col-xs-8 col-xs-offset-2 visible-xs'><br><p class='thankyou'>Thank you!</p></div>");
   });
  });
