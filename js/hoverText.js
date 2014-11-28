648f1a098e03911a0cc30dd60753e907b09b54b3
Timothy Achumba <tim@6wunderkinder.com>
2014-11-28 15:13:34 +0100

    restructured the boxes and started styling them

--- js/hoverText.js
+++ js/hoverText.js
@@ -1,335 +1,335 @@
-$('#box1').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Take a friend out for coffee, ask how they are doing & take time to really listen.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box1').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Take a friend out for coffee, ask how they are doing & take time to really listen.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
 
-/* BOX 2 */
+// /* BOX 2 */
 
-$('#box2').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Handwrite a note of encouragement to someone and put it in their bag without them noticing.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box2').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Handwrite a note of encouragement to someone and put it in their bag without them noticing.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 3 */
+// /* BOX 3 */
 
-$('#box3').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Make time to call your grandparents or other family members.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box3').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Make time to call your grandparents or other family members.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 4 */
+// /* BOX 4 */
 
-$('#box4').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Smile. Just because. It can change someone's day.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box4').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Smile. Just because. It can change someone's day.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 5 */
+// /* BOX 5 */
 
-$('#box5').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Put a chocolate 'Nikolaus' in front of your neighbour's door right before the 6th.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box5').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Put a chocolate 'Nikolaus' in front of your neighbour's door right before the 6th.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 6 */
+// /* BOX 6 */
 
-$('#box6').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Treat a homeless person to a hot chocolate.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box6').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Treat a homeless person to a hot chocolate.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 7 */
+// /* BOX 7 */
 
-$('#box7').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Bless someone with a book you love. Write a kind note in the front.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box7').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Bless someone with a book you love. Write a kind note in the front.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 8 */
+// /* BOX 8 */
 
-$('#box8').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Leave a generous tip for a service person.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box8').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Leave a generous tip for a service person.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 9 */
+// /* BOX 9 */
 
-$('#box9').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Put a sticky note with a message of value on a restroom mirror.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box9').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Put a sticky note with a message of value on a restroom mirror.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 10 */
+// /* BOX 10 */
 
-$('#box10').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Write someone a letter rather than just facebooking or text messaging them.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box10').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Write someone a letter rather than just facebooking or text messaging them.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 11 */
+// /* BOX 11 */
 
-$('#box11').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Donate clothes to one of the of the many initiatives in Berlin.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box11').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Donate clothes to one of the of the many initiatives in Berlin.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 12 */
+// /* BOX 12 */
 
-$('#box12').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("If you are in a waiting line, let someone get ahead of you. ");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box12').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("If you are in a waiting line, let someone get ahead of you. ");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 13 */
+// /* BOX 13 */
 
-$('#box13').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Bake cookies for your work colleagues, classmates or a homeless person.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box13').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Bake cookies for your work colleagues, classmates or a homeless person.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 14 */
+// /* BOX 14 */
 
-$('#box14').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Pay in advance for the next person behind you in the line at a coffee shop.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box14').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Pay in advance for the next person behind you in the line at a coffee shop.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 15 */
+// /* BOX 15 */
 
-$('#box15').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Hold the door open for someone.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box15').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Hold the door open for someone.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 16 */
+// /* BOX 16 */
 
-$('#box16').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Surprise someone with flowers who did not expect it.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box16').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Surprise someone with flowers who did not expect it.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 17 */
+// /* BOX 17 */
 
-$('#box17').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Cook a meal for a homeless person.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box17').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Cook a meal for a homeless person.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
 
-/* BOX 18 */
+// /* BOX 18 */
 
-$('#box18').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Bring a box of chocolates for your bus driver.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box18').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Bring a box of chocolates for your bus driver.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 19 */
+// /* BOX 19 */
 
-$('#box19').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Leave your change in a vending machine.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box19').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Leave your change in a vending machine.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 20 */
+// /* BOX 20 */
 
-$('#box20').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Leave your change in a vending machine.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box20').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Leave your change in a vending machine.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 21 */
+// /* BOX 21 */
 
-$('#box21').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Reconnect with an old friend you haven't seen in a while.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box21').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Reconnect with an old friend you haven't seen in a while.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 22 */
+// /* BOX 22 */
 
-$('#box22').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Treat a friend or family member to dinner at your place.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box22').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Treat a friend or family member to dinner at your place.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 23 */
+// /* BOX 23 */
 
-$('#box23').hover(
-    function() {
-        var $this = $(this); 
-        $this.data('initialText', $this.text());
-        $this.text("Place a christmas card on someone's car wishing them a blessed season.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box23').hover(
+//     function() {
+//         var $this = $(this);
+//         $this.data('initialText', $this.text());
+//         $this.text("Place a christmas card on someone's car wishing them a blessed season.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
 
-/* BOX 24 */
+// /* BOX 24 */
 
-$('#box24').hover(
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Be thankful.");
-    },
-    function() {
-        var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
-    }
-);
+// $('#box24').hover(
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.data('initialText', $this.text());
+//         $this.text("Be thankful.");
+//     },
+//     function() {
+//         var $this = $(this); // caching $(this)
+//         $this.text($this.data('initialText'));
+//     }
+// );
