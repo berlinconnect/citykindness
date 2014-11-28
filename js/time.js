313abcc95b28370261987ff6952065da647291da
pascal hausberger <hausberger.pascal@gmail.com>
2014-11-28 15:45:40 +0100

    Time check
    added time.js to check the date

---
+++ js/time.js
@@ -0,0 +1,14 @@
+$(function() {
+    var d = new Date();
+	var day = d.getDate();
+	var month = d.getMonth();
+	var i = 0;
+
+	if(month == 10){
+		while( i < (day+1) ){
+			var boxNumber = "#box" + i;
+			$(boxNumber).addClass("hidden");
+			i++;
+		}
+	}
+});
