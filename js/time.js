991798d09216506bfc9ea7bf60be616f13917fb7
Timothy Achumba <tim@6wunderkinder.com>
2014-11-28 16:10:06 +0100

    Fixes

--- js/time.js
+++ js/time.js
@@ -2,12 +2,12 @@ $(function() {
     var d = new Date();
 	var day = d.getDate();
 	var month = d.getMonth();
-	var i = 0;
+	var i = 1;
 
 	if(month == 10){
 		while( i < (day+1) ){
-			var boxNumber = "#box" + i;
-			$(boxNumber .content).removeClass("hidden");
+			var boxNumber = "#box" + i + " .content";
+			$(boxNumber).removeClass("hidden");
 			i++;
 		}
 	}
