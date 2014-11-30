7ffb3525e84cfa53e98de6d42e054be23b9b9eee
pascal hausberger <hausberger.pascal@gmail.com>
2014-11-30 17:29:53 +0100

    added some images

--- js/time.js
+++ js/time.js
@@ -1,10 +1,10 @@
 $(function() {
-    var d = new Date();
+    var d = new Date("");
 	var day = d.getDate();
 	var month = d.getMonth();
 	var i = 1;
 
-	if(month == 10){
+	if(month == 11){
 		while( i < (day+1) ){
 			var boxNumber = "#box" + i + " .content";
 			$(boxNumber).removeClass("hidden");
