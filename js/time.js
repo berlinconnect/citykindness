33b89ea0c45a9e836ce8e27c437c369dbb90b7b3
hauspa <pasci@pascals-MacBook-Pro.local>
2015-01-29 18:46:57 +0100

    Removed Stuff The Bus & fixed js error
    our js was only intended for december, now it will work all the time.
    just in case we want to reuse the code next christmas, i’d left the
    code in a comment.

--- js/time.js
+++ js/time.js
@@ -4,11 +4,23 @@ $(function() {
 	var month = d.getMonth();
 	var i = 1;
 
-	if(month == 11){
-		while( i < (day+1) ){
-			var boxNumber = "#box" + i + " .content";
-			$(boxNumber).removeClass("hideme");
-			i++;
-		}
+	// Since the campaign is already over
+	// and we are showing all pictures:
+	while( i < (25) ){
+		var boxNumber = "#box" + i + " .content";
+		$(boxNumber).removeClass("hideme");
+		i++;
 	}
+	
+
+	// The following snippet was only valid for December
+
+	// if(month == 11){
+	// 	while( i < (day+1) ){
+	// 		var boxNumber = "#box" + i + " .content";
+	// 		$(boxNumber).removeClass("hideme");
+	// 		i++;
+	// 	}
+	// }
+
 });
